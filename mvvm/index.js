df15ad1403f2df4be84f148b362704b621d5f72c
dwshen <1361959119@qq.com>
2021-11-07 22:28:11 +0800

    双向数据绑定学习

---
+++ mvvm/index.js
@@ -0,0 +1,27 @@
+let obj = {};
+Object.defineProperty(obj, 'school', {
+    // 可配置，那么就可以被删除
+    configurable: true,
+    // 可写
+    writable: true,
+    // 可枚举
+    enumerable: true,
+    value: 'zfpx'
+});
+
+// 删除属性
+delete obj.school;
+
+// 枚举所有属性
+for (const key in obj) {
+    console.log(key);
+}
+
+// vue特点不能新增不存在的属性，不能存在的属性没有get和set
+
+
+// 什么是深度响应
+// 深度响应，因为每次赋予一个新对象
+
+
+
